7d0bd24550e0cb7c8ed8cea47961b2638936a0a5
delabahmani <delabahmani@gmail.com>
2024-10-14 15:33:44 -0400

    nextauth linked to prisma and mongo

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
 
 const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,36 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import NavbarItem from "./NavbarItem";
 import Button from "../universal/Button";
 import { useSession, signOut } from "next-auth/react";
 
 export default function Navbar() {
-  const { status } = useSession();
+  const { status, data: session } = useSession();
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const popupRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (popupRef.current && !popupRef.current.contains(e.target as Node)) {
+        setIsPopupVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    if (!isPopupVisible) {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isPopupVisible]);
 
   return (
-    <header className="h-16 p-6 mb-4 items-center border-b w-[70%] mx-auto">
+    <header className="h-16 p-6 mb-4 items-center border-b w-[70%] mx-auto relative">
       <nav className="flex justify-between items-center">
         <div className="items-center flex">
           <Link href={"/"} className="">
@@ -33,15 +53,31 @@ export default function Navbar() {
         {/* login/sign up avatar */}
 
         {status === "authenticated" ? (
-          <div>
-            <Button
-              text="sign out"
-              bgColor="bg-orange"
-              textColor="text-offwhite"
-              textSize="text-sm"
-              textWeight="font-semibold"
-              width="w-[90px]"
-              onClick={() => signOut()}
+          <div ref={popupRef}>
+            <div
+              className={`absolute z-30 right-0 top-20 bg-white p-6 shaodw-lg rounded-md  flex-col gap-2 text-right min-w-[160px] ${
+                isPopupVisible ? "flex" : "hidden"
+              }`}
+            >
+              <div className="font-bold">{session?.user?.name}</div>
+              <Button
+                text="sign out"
+                bgColor="bg-orange"
+                textColor="text-offwhite"
+                textSize="text-sm"
+                textWeight="font-semibold"
+                width="w-[90px]"
+                onClick={() => signOut()}
+              />
+            </div>
+
+            <Image
+              src={session?.user?.image || ""}
+              width={36}
+              height={36}
+              alt="profile image"
+              className="rounded-full cursor-pointer"
+              onClick={() => setIsPopupVisible((prev) => !prev)}
             />
           </div>
         ) : (
